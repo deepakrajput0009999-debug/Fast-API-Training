56fafedd00e0be2ce3cdc18f6fb17ea244f37013
Deepak rajput <deepakrajput0009999@gamil.com>
2026-09-22 13:29:19 +0530

    path parameters with type int

--- Day/day2/FASTAPIPractice/main.py
+++ Day/day2/FASTAPIPractice/main.py
@@ -15,3 +15,7 @@ def create_something():
 @app.get("/student/{usn}")
 def get_result(usn):
     return {"Result":"Distinction","usn":usn}
+# path parameters with type int
+@app.get("/candidate/{rollno}")
+def get_result(rollno:int):
+    return {"Result":"Distinction","rollno":rollno,"type":str(type(rollno))}
